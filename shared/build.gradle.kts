4b8e29dbdff465cc6ac13a70e70a4f271474e5c7
machi <kmachida12345@gmail.com>
2021-06-11 15:40:55 +0900

    FirebaseKotlinSDKの依存関係を追加
    https://github.com/gitliveapp/firebase-kotlin-sdk/

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -28,7 +28,11 @@ kotlin {
     }
     
     sourceSets {
-        val commonMain by getting
+        val commonMain by getting {
+            dependencies {
+                implementation("dev.gitlive:firebase-firestore:1.3.1")
+            }
+        }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test-common"))
